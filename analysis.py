715d16db99905a8c47474dcdb585d95e90cb3c18
m-nez <mn@openmailbox.org>
2018-06-13 15:33:59 +0200

    Improved docstring for analyse

--- analysis.py
+++ analysis.py
@@ -1,10 +1,23 @@
 def reweight(df):
     """
     Multiply columns by scalar to modify their influence on the distance
+
+    df : pandas.DataFrame
     """
     return df
 
 def analyse(df):
+    """
+    Return the predicted true share price
+
+    df : pandas.DataFrame
+        First row contains the values for the target company
+        which share price is to be predicted.
+        Other rows contain values for companies
+        which are similar to the targe company.
+
+        df["Kurs"][0] is the share price of the target company
+    """
     return 1
 
 
