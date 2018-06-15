ce9f3472a3932cec753f4f10c0f8a9b185526255
kok00n <kamilkonat@gmail.com>
2018-06-15 04:22:13 +0200

    I used exponential smooth function in the file also valuated company

--- value_determination.py
+++ value_determination.py
@@ -6,7 +6,8 @@ from sklearn.preprocessing import minmax_scale
 from pyspark.sql import SparkSession
 from pyspark import SparkContext
 #import matplotlib.pyplot as plt
-from analysis import analyse, reweight, summarise
+from analysis import analyse, reweight, summarise, esmooth_df
+
 
 def task(quarter, k):
     """
@@ -29,15 +30,18 @@ def task(quarter, k):
         knearest = pandas.DataFrame(
                 quarter.values[indices],
                 columns=quarter.columns)
+
         results.append(analyse(knearest))
 
     return results
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("files", nargs="+", help="csv files with company data")
     parser.add_argument("-m", "--master", default="local")
     parser.add_argument("-k", default=5, help="k nearest companies to take for analysis")
+
     args = parser.parse_args()
 
     sc = SparkContext(
@@ -61,7 +65,17 @@ if __name__ == "__main__":
         quarters.append(pandas.concat([df.iloc[-i, 1:] for df in values], axis=1).T)
 
     quartersRDD = sc.parallelize(quarters)
-    results = quartersRDD.map(lambda x : task(x, args.k)).collect()
+    results = quartersRDD.map(lambda x : task(x, int(args.k))).collect()
+
+    # Exponential smoothing
+    series = list()
+    for itr in range(len(results[0])):
+        series.append(esmooth_df(results, 'Median_P2E', minrows, itr, 0.9))
+
+        # Exact valuation
+        results[itr][0]['Median_P2E'] = series[itr].iloc[0,0]
+        results[itr][0]['Kurs'] = results[itr][0]['Median_P2E'] * results[itr][0]['Zysk netto']
+
     summarise(results)
 
     spark.stop()
